56c5789f716c5ba48c39f3f1a93c76591772aabe
donb01326-hue <donb01326@gmail.com>
2026-01-06 13:13:21 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -116,12 +116,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">6332102413</p>
+            <p className="text-lg font-bold">5136743923</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("6332102413", "Account Number")}
+              onClick={() => handleCopy("5136743923", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -136,7 +136,7 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">OBADIAH RAPHAEL</p>
+          <p className="text-lg font-bold">AROWOJOLU PIUS</p>
         </div>
       </div>
 
